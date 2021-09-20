0576c2c9e1a764282c5db11ca2af77d9d14291f2
Stephane Gilles <sag48@njit.edu>
2021-09-19 22:24:35 -0400

    adding testing php file + hw2 templates

---
+++ public_html/my_new_db.php
@@ -0,0 +1,33 @@
+/* this is a comment... hello world + other things */<br>
+<script>
+    console.log("you can see the console... hi!")
+    alert("hi!")
+</script>
+
+<?php
+echo "Hello World!<br>";
+ECHO "Hello World!<br>";
+EcHo "Hello World!<br>";
+print "Hello World! but with print<br>";
+
+$myVar = "Hello from a variable<br>";
+
+$x = PHP_INT_MAX;
+$y = PHP_FLOAT_MAX;
+
+echo $myVar;
+
+echo "max int = $x<br>";
+echo "max float = $y<br>";
+var_dump($myVar);
+
+$myDict = [1=>"one"];
+
+echo "<br>";
+
+$z = 0.3;
+
+var_export($z);
+echo "<br>";
+var_dump($myDict);
+?>
