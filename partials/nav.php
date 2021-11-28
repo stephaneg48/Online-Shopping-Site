20082997fcf70344fffc2fdd35e6874dba207e6f
Stephane Gilles <sag48@njit.edu>
2021-11-28 18:28:38 -0500

    basic shop page, nav.php updated accordingly

--- partials/nav.php
+++ partials/nav.php
@@ -33,6 +33,7 @@ require_once(__DIR__ . "/../lib/functions.php");
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
+            <li><a href="<?php echo get_url('shop.php'); ?>">Shop</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
---
+++ public_html/Project/shop.php
@@ -0,0 +1,47 @@
+<?php
+require(__DIR__ . "/../../partials/nav.php");
+
+$results = [];
+$db = getDB();
+$stmt = $db->prepare("SELECT id, name, description, category, stock, unit_price, visibility FROM Products WHERE visibility = 1 AND stock > 0 LIMIT 99");
+try {
+    $stmt->execute();
+    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($r) {
+        $results = $r;
+    }
+} catch (PDOException $e) {
+    flash("<pre>" . var_export($e, true) . "</pre>");
+}
+?>
+
+<script>
+    function purchase(item) {
+        console.log("TODO purchase item", item);
+        //TODO create JS helper to update all show-balance elements
+    }
+</script>
+
+<div class="container-fluid">
+    <h1>Shop</h1>
+    <div class="row row-cols-1 row-cols-md-5 g-4">
+        <?php foreach ($results as $item) : ?>
+            <div class="col">
+                <div class="card bg-light">
+                    <div class="card-header">
+                    <h5 class="card-title"><?php se($item, "category"); ?></h5>
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title"><?php se($item, "name"); ?></h5>
+                        <p class="card-text"><?php se($item, "description"); ?></p>
+                    </div>
+                    <div class="card-footer">
+                        Cost: <?php se($item, "unit_price"); ?>
+                        <button onclick="purchase('<?php se($item, 'id'); ?>')" class="btn btn-primary">Purchase</button>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
