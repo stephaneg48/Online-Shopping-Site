697517af9a161401772406f12b0287b8a64ffdb2
Stephane Gilles <sag48@njit.edu>
2021-12-02 13:45:15 -0500

    starting to add filtering, attempting to make edit product button redirect to the page with the product name as data to use as a filter

--- public_html/Project/shop.php
+++ public_html/Project/shop.php
@@ -24,6 +24,45 @@ try {
 
 <div class="container-fluid">
     <h1>Shop</h1>
+    <form class="row row-cols-auto g-3 align-items-center">
+        <div class="col">
+            <div class="input-group">
+                <div class="input-group-text">Search</div>
+                <input class="form-control" name="item" value="<?php se($results); ?>" />
+            </div>
+        </div>
+        <div class="col">
+            <div class="input-group">
+                <div class="input-group-text">Sort</div>
+                <!-- make sure these match the in_array filter above-->
+                <select class="form-control" name="col" value="<?php se($col); ?>">
+                    <option value="cost">Cost</option>
+                    <option value="stock">Stock</option>
+                    <option value="name">Name</option>
+                    <option value="created">Created</option>
+                </select>
+                <script>
+                    //quick fix to ensure proper value is selected since
+                    //value setting only works after the options are defined and php has the value set prior
+                    document.forms[0].col.value = "<?php se($col); ?>";
+                </script>
+                <select class="form-control" name="order" value="<?php se($order); ?>">
+                    <option value="asc">Up</option>
+                    <option value="desc">Down</option>
+                </select>
+                <script>
+                    //quick fix to ensure proper value is selected since
+                    //value setting only works after the options are defined and php has the value set prior
+                    document.forms[0].order.value = "<?php se($order); ?>";
+                </script>
+            </div>
+        </div>
+        <div class="col">
+            <div class="input-group">
+                <input type="submit" class="btn btn-primary" value="Apply" />
+            </div>
+        </div>
+    </form>
     <div class="row row-cols-1 row-cols-md-5 g-4">
         <?php foreach ($results as $item) : ?>
             <div class="col">
@@ -40,6 +79,9 @@ try {
                         Cost: <?php se($item, "unit_price"); ?>
                         <button onclick="purchase('<?php se($item, 'id'); ?>')" class="btn btn-primary">Add to Cart</button>
                         <?php if (has_role("Admin") || has_role("Shop Owner")) : ?>
+                            <form>
+
+                            </form>
                             <a href="<?php echo get_url('admin/edit_product.php'); ?>" class="btn btn-primary">Edit Product</a> 
                             <!-- the class makes it look like a button here -->
                         <?php endif; ?>
