060a991b27781c257784c9f8f0ba8c8d1fc1a127
Stephane Gilles <sag48@njit.edu>
2021-12-06 23:59:57 -0500

    login check, flash addition, basic header

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -2,8 +2,21 @@
 require(__DIR__ . "/../../partials/nav.php");
 ?>
 
+<?php
+if (!is_logged_in()) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: login.php"));
+}
 
+// ...
+
+?>
+
+<div class="container-fluid">
+    <h1>Your Cart</h1>
+</div>
 
 <?php
 require(__DIR__."/../../partials/flash.php");
 ?>
+
